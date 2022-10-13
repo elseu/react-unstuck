e878f01074b8e7612c90dbf16f3349937a34b7f6
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2022-10-13 16:09:54 +0200

    Make releaserc conform to Lennard's version

--- .releaserc.js
+++ .releaserc.js
@@ -1,10 +1,20 @@
 module.exports = {
   branches: [
-    "master",
     "main",
+    "master",
     {
       name: "develop",
       prerelease: true
+    },
+    {
+      name: "release",
+      prerelease: true
     }
+  ],
+  plugins: [
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator"
   ]
 };
